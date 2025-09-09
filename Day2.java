cd05b7a33d5e62def5e9fa351272cb100f07b8ef
sikuning0001-cpu <sikuning0001@gmail.com>
2025-09-09 08:16:25 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,5 @@
+public class  {
+public static void main(String[] args) {
+        System.out.print("Saya Ari");
+        System.out.println("Aku Bernama Ari");
+        System.out.printf("Nama saya %s, Umur saya %d", "Arif Tampan", 18);
