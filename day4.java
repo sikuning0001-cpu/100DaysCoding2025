8683fab30cd9dfa27c41ac1510144d884ced1913
sikuning0001-cpu <sikuning0001@gmail.com>
2025-09-11 05:25:50 +0800

    Create day4.java

---
+++ day4.java
@@ -0,0 +1,12 @@
+public class orangtampan {
+
+    public static void main(String[] args) {
+        System.out.println("nama saya \t: muh. arif");
+        System.out.print("asal saya \t: puawang");
+        System.out.printf("\nnama saya \t: %s \numur saya \t: %d", "muh. arif", 18);
+//        ini adalah kodingan saya untuk day 2 tapi day 3 itu disuruh untuk membuat sebuah 
+//        komentar di dalam codingan nah jadi kalimat ini akan menjadi sebuah komentar saya
+//        yang akan menyelasaikan tugas di day 3 eh untuk day 4 pale juga ini kak 
+    }
+
+}
