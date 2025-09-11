dc03083ee96c8a76bdd0cd032ea5965e5d71ef8b
sikuning0001-cpu <sikuning0001@gmail.com>
2025-09-12 00:00:07 +0800

    Create day5.java

---
+++ day5.java
@@ -0,0 +1,15 @@
+public class FORISC100DAYSCODING {
+
+    public static void main(String[] args) {
+        byte a = 100;
+        short b = 2000;
+        int c = 3000;
+        long d = 1000000000L ;
+        
+        System.out.println("byte \t:" + a);
+        System.out.println("short \t:" + b);
+        System.out.println("int \t:" + c);
+        System.out.println("long \t:" + d);
+    }
+
+}
