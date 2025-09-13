4f0b3a475c32fea6802513623ebf2f814c9709a3
sikuning0001-cpu <sikuning0001@gmail.com>
2025-09-13 08:32:35 +0800

    Create day6.java

---
+++ day6.java
@@ -0,0 +1,13 @@
+public class Day6 {
+    public static void main(String[] args) {
+        // float → angka desimal presisi 7 digit
+        float x = 3.14f;  // pakai 'f' di akhir biar dikenali float
+
+        // double → angka desimal presisi 15 digit
+        double y = 3.14159265359;
+
+        // Cetak hasil
+        System.out.println("float: " + x);
+        System.out.println("double: " + y);
+    }
+}
