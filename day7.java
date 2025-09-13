f8030310957bcb43e2bb74e76f8af2ad29551098
sikuning0001-cpu <sikuning0001@gmail.com>
2025-09-14 00:00:07 +0800

    Create day7.java

---
+++ day7.java
@@ -0,0 +1,12 @@
+public class Day7 {
+    public static void main(String[] args) {
+        // char → menyimpan 1 karakter
+        char grade = 'A';
+
+        // boolean → menyimpan nilai benar/salah
+        boolean isPassed = true;
+
+        System.out.println("Grade: " + grade);
+        System.out.println("Lulus? " + isPassed);
+    }
+}
