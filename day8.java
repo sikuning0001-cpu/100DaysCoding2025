2a27eb2545d3ba6a194d26163feac132670aa9f2
sikuning0001-cpu <sikuning0001@gmail.com>
2025-09-15 10:31:25 +0800

    Create day8.java

---
+++ day8.java
@@ -0,0 +1,10 @@
+public class Day8 {
+    public static void main(String[] args) {
+        String nama = "Ari Ganteng";  // String untuk teks
+
+        // Beberapa operasi pada string
+        System.out.println("Nama: " + nama);
+        System.out.println("Jumlah huruf: " + nama.length()); // hitung panjang
+        System.out.println("Huruf besar: " + nama.toUpperCase()); // ubah ke kapital
+    }
+}
