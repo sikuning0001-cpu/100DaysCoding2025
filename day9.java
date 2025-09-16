a29bbe8309c71a50cccf25d17fdc0bc5b1e12036
sikuning0001-cpu <sikuning0001@gmail.com>
2025-09-16 20:55:03 +0800

    Create day9.java

---
+++ day9.java
@@ -0,0 +1,13 @@
+public class Day9 {
+    public static void main(String[] args) {
+        // Deklarasi + Inisialisasi
+        int umur = 18;  // deklarasi (int) + inisialisasi (18)
+
+        System.out.println("Umur awal: " + umur);
+
+        // Update nilai variabel
+        umur = 19;
+
+        System.out.println("Umur setelah update: " + umur);
+    }
+}
