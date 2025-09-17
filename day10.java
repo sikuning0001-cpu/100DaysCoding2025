aa59f7f2396697ce760d974794f2cb71c15a8621
sikuning0001-cpu <sikuning0001@gmail.com>
2025-09-17 18:22:43 +0800

    Update day10.java

--- day10.java
+++ day10.java
@@ -1,6 +1,6 @@
 public class Day10 {
     public static void main(String[] args) {
-        final double PI = 3.2007;  // final → nilainya tidak bisa diubah
+        final double PI = 3.2007;  // final  nilainya tidak bisa diubah
         System.out.println("Nilai PI: " + PI);
     }
 }
