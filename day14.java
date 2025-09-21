3802575fdc4c255070a07bb2847e8a273f319981
sikuning0001-cpu <sikuning0001@gmail.com>
2025-09-21 19:20:02 +0800

    Create day14.java

---
+++ day14.java
@@ -0,0 +1,9 @@
+public class Day14 {
+    public static void main(String[] args) {
+        int angka = Integer.parseInt("123");
+        double pecahan = Double.parseDouble("3.14");
+
+        System.out.println("int: " + angka);
+        System.out.println("double: " + pecahan);
+    }
+}
