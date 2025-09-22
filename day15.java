dcbb5af9fd246725c1ba32de2592e69ff9570cfa
sikuning0001-cpu <sikuning0001@gmail.com>
2025-09-22 19:53:17 +0800

    Create day15.java

---
+++ day15.java
@@ -0,0 +1,8 @@
+public class Day15to17 {
+    public static void main(String[] args) {
+        int a = 10, b = 3;
+
+        System.out.println("Tambah: " + (a + b));  // penjumlahan
+        System.out.println("Kurang: " + (a - b));  // pengurangan
+    }
+}
