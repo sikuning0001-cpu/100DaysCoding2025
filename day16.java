f46db4508e4f8de49bf7ec6d1cd795dd27388ec0
sikuning0001-cpu <sikuning0001@gmail.com>
2025-09-23 09:32:46 +0800

    Create day16.java

---
+++ day16.java
@@ -0,0 +1,10 @@
+public class Day15to17 {
+    public static void main(String[] args) {
+        
+        int a = 10, b = 3;
+
+        System.out.println("Kali: " + (a * b));
+        System.out.println("Bagi: " + (a / b));
+        
+    }
+}
