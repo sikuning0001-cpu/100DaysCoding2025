6bd46ab939973bd2882b1a020ab58da4cd6e1e02
sikuning0001-cpu <sikuning0001@gmail.com>
2025-09-25 20:44:29 +0800

    Create day18.java

---
+++ day18.java
@@ -0,0 +1,9 @@
+public class Day18 {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = a; // otomatis int double
+
+        System.out.println("int: " + a);
+        System.out.println("double: " + b);
+    }
+}
