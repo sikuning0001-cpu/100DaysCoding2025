8104513a6bcd5a343c083c2973539c3667b9119a
sikuning0001-cpu <sikuning0001@gmail.com>
2025-09-25 20:47:58 +0800

    Update day18.java

--- day18.java
+++ day18.java
@@ -1,9 +1,9 @@
 public class Day18 {
     public static void main(String[] args) {
-        int a = 10;
-        double b = a; // otomatis int double
+        byte a = 10;
+        int b = a; // otomatis int double
 
-        System.out.println("int: " + a);
-        System.out.println("double: " + b);
+        System.out.println("byte: " + a);
+        System.out.println("int: " + b);
     }
 }
