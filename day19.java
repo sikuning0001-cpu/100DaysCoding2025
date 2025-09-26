e9f82a7d1f67dd750f50b85f35460a358be1f75b
sikuning0001-cpu <sikuning0001@gmail.com>
2025-09-26 22:19:31 +0800

    Update day19.java

--- day19.java
+++ day19.java
@@ -1,7 +1,7 @@
 public class Day19 {
     public static void main(String[] args) {
         long a = 2008;
-        short b = (int) a; // casting manual
+        short b = (short) a; // casting manual
 
         System.out.println("long : " + a);
         System.out.println("short : " + b);
