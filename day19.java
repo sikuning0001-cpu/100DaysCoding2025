03f03e8ef2e06bb61563f8c19623dc6face71f6a
sikuning0001-cpu <sikuning0001@gmail.com>
2025-09-26 22:16:02 +0800

    Create day19.java

---
+++ day19.java
@@ -0,0 +1,9 @@
+public class Day19 {
+    public static void main(String[] args) {
+        long a = 2008;
+        short b = (int) a; // casting manual
+
+        System.out.println("long : " + a);
+        System.out.println("short : " + b);
+    }
+}
