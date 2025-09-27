31903e027a2c50a1869f32fd526a7f7e91d96fea
sikuning0001-cpu <sikuning0001@gmail.com>
2025-09-27 21:44:08 +0800

    Create day20.java

---
+++ day20.java
@@ -0,0 +1,8 @@
+public class Day20 {
+    public static void main(String[] args) {
+        int umur = 18;
+        String teks = String.valueOf(umur);
+
+        System.out.println("String: " + teks);
+    }
+}
