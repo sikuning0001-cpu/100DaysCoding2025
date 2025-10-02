ef29519b795da7fc44912bfc19ee535d3f6fffb5
sikuning0001-cpu <sikuning0001@gmail.com>
2025-10-02 22:47:13 +0800

    Create day25.java

---
+++ day25.java
@@ -0,0 +1,12 @@
+public class Day25 {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println("Nilai awal: " + x);
+
+        x++; 
+        System.out.println("Setelah increment: " + x);
+
+        x--; 
+        System.out.println("Setelah decrement: " + x);
+    }
+}
