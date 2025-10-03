b94c9f4a01f1754ca15524dc6a134329826423b9
sikuning0001-cpu <sikuning0001@gmail.com>
2025-10-03 23:21:32 +0800

    Create day26.java

---
+++ day26.java
@@ -0,0 +1,20 @@
+public class Day26 {
+    public static void main(String[] args) {
+        int x = 10;
+
+        x += 5;
+        System.out.println("x += 5 → " + x);
+
+        x -= 3;
+        System.out.println("x -= 3 → " + x);
+
+        x *= 2;
+        System.out.println("x *= 2 → " + x);
+
+        x /= 4;
+        System.out.println("x /= 4 → " + x);
+
+        x %= 3;
+        System.out.println("x %= 3 → " + x);
+    }
+}
