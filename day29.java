4dbe9a34f6740c8a5681a23a5e06822a978d75be
KingAriii <sikuning0001@gmail.com>
2025-10-06 22:15:10 +0800

    Create day29.java

---
+++ day29.java
@@ -0,0 +1,12 @@
+public class PerbandinganTanpaIfDanSama {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b + "\n");
+
+        System.out.println("Apakah a lebih besar dari b? " + (a > b));
+        System.out.println("Apakah a lebih kecil dari b? " + (a < b));
+    }
+}
