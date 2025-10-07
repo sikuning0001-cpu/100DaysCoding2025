1789356b5eeb26499ddf0a4eedf1472d0b38e319
KingAriii <sikuning0001@gmail.com>
2025-10-07 17:57:28 +0800

    Create day30.java

---
+++ day30.java
@@ -0,0 +1,12 @@
+public class Day30OperatorKurangLebihSama {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b + "\n");
+
+        System.out.println("Apakah a >= b ? " + (a >= b));
+        System.out.println("Apakah a <= b ? " + (a <= b));
+    }
+}
