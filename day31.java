79fe3125d5a48bfed415e42f37811588cb3fe066
KingAriii <sikuning0001@gmail.com>
2025-10-08 19:45:46 +0800

    Create day31.java

---
+++ day31.java
@@ -0,0 +1,10 @@
+public class Day31 {
+    public static void main(String[] args) {
+        int teori = 80;
+        int praktek = 70;
+
+        boolean lulus = (teori >= 75) && (praktek >= 75);
+
+        System.out.println("Lulus ujian: " + lulus);
+    }
+}
