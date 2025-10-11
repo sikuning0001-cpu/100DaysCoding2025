390dd5dc0c2d0d08869d88466036b606802b87be
KingAriii <sikuning0001@gmail.com>
2025-10-11 19:30:05 +0800

    Create day34.java

---
+++ day34.java
@@ -0,0 +1,9 @@
+public class Day34 {
+    public static void main(String[] args) {
+        int a = 10, b = 5, c = 2;
+
+        boolean hasil = (a + b * c > 20) && (a - b < c * 3);
+
+        System.out.println("Hasil logika = " + hasil);
+    }
+}
