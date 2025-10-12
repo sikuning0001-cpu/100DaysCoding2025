d6d60f9b78c1e03398f3c81953c28f41ffa3b2d4
KingAriii <sikuning0001@gmail.com>
2025-10-12 20:18:32 +0800

    Create day35.java

---
+++ day35.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int nilai = 85;
+
+        if (nilai >= 75) {
+            System.out.println("Kamu Lulus!");
+        }
+
+        System.out.println("Program selesai.");
+    }
+}
