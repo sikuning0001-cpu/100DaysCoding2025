3b49d44fd83f26a83fd28ea3fced1788bf14623c
KingAriii <sikuning0001@gmail.com>
2025-10-19 21:42:23 +0800

    Update day42.java

--- day42.java
+++ day42.java
@@ -1,6 +1,8 @@
+package KerjaSoal;
+
 import java.util.Scanner;
 
-public class GajiKaryawan {
+public class Soal3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double gajiPokok, tunjangan = 0, bonus = 0, potongan, totalGaji;
@@ -15,21 +17,26 @@ public class GajiKaryawan {
 
         System.out.print("Apakah mendapat tunjangan? (y/n): ");
         statusTunjangan = input.next().charAt(0);
-        if (statusTunjangan == 'y' || statusTunjangan == 'Y')
-            System.out.print("Masukkan jumlah tunjangan: "), tunjangan = input.nextDouble();
-        else
+        if (statusTunjangan == 'y' || statusTunjangan == 'Y') {
+            System.out.print("Masukkan jumlah tunjangan: ");
+            tunjangan = input.nextDouble();
+        } else {
             tunjangan = 0;
+        }
 
         System.out.print("Apakah mendapat bonus? (y/n): ");
         statusBonus = input.next().charAt(0);
-        if (statusBonus == 'y' || statusBonus == 'Y')
-            System.out.print("Masukkan jumlah bonus: "), bonus = input.nextDouble();
-        else
+        if (statusBonus == 'y' || statusBonus == 'Y') {
+            System.out.print("Masukkan jumlah bonus: ");
+            bonus = input.nextDouble();
+        } else {
             bonus = 0;
+        }
 
         System.out.print("Masukkan potongan: ");
         potongan = input.nextDouble();
 
+   
         if (tunjangan > 0 && bonus > 0)
             System.out.println("Karyawan mendapat tunjangan dan bonus.");
         else if (tunjangan > 0)
