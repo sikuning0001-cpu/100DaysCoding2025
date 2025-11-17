55cf28f7d09ccd00d400e442a99d8345a391c5f2
KingAriii <sikuning0001@gmail.com>
2025-11-17 20:07:02 +0800

    Update day71.java

--- day71.java
+++ day71.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
-public class Hayyuk {
+public class Segitiga {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        for (int i = 1; i <= a; i++) {
-            for (int j = i; j <= a; j++) {
+
+        System.out.print("Masukkan tinggi segitiga: ");
+        int n = in.nextInt();
+
+        for (int i = 1; i <= n; i++) {     // baris
+            for (int j = 1; j <= i; j++) { // jumlah bintang per baris
                 System.out.print("*");
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 }
